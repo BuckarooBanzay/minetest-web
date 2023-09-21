07e831438c3fbe28f94c6d83339030a654b171cf
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-09-21 09:00:30 +0200

    serverlist wip

--- public/js/routes.js
+++ public/js/routes.js
@@ -1,5 +1,8 @@
 import Login from "./components/Login.js";
+import Serverlist from "./components/Serverlist.js";
 
 export default [{
     path: "/", component: Login
+},{
+    path: "/serverlist", component: Serverlist
 }];
